16a4507fd5481e7286544069d9cdcea3963f93fc
Tanveer Hussain <tanveer@gkmit.co>
2025-11-13 11:50:53 +0530

    refactor(urls): Update API route for authentication endpoints

--- accipere/urls.py
+++ accipere/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls')),
+    path('api/auth/', include('authentication.urls')),
     path('api/', include('common.urls')),
     path('api/', include('jobs.urls')),
     path('api/', include('users.urls')),
