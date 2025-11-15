c8bdbbb37416550789698e4ca1240dba30d207b2
tanveer-gkmit <tanveer@gkmit.co>
2025-11-15 11:02:09 +0530

    Implement Job model and related all api endpoints .  (#5)

--- common/models.py
+++ common/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class SoftDeleteManager(models.Manager):
+    """Manager that excludes soft-deleted objects by default."""
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+
+class SoftDeleteModel(models.Model):
+    """Abstract base model that provides soft delete functionality."""
+    
+    deleted_at = models.DateTimeField(null=True, blank=True, db_index=True)
+    
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()  # Access all objects including deleted
+    
+    class Meta:
+        abstract = True
+    
+    def delete(self, using=None, keep_parents=False, hard=False):
+        """
+        Soft delete by default. Use hard=True for permanent deletion.
+        """
+        if hard:
+            return super().delete(using=using, keep_parents=keep_parents)
+        
+        self.deleted_at = timezone.now()
+        self.save(using=using)
+    
+    def is_deleted(self):
+        """Check if object is soft-deleted."""
+        return self.deleted_at is not None
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .serializers import JobSerializer
+from .models import Jobs
+from rest_framework import viewsets
+from common.permissions import IsRecruiter, IsAdmin 
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+
+class JobViewset(viewsets.ModelViewSet):
+    queryset = Jobs.objects.all()
+    serializer_class = JobSerializer
+    
+    def get_permissions(self):
+        """
+        Set permissions based on action.
+        """
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny()]
+        elif self.action in ['create', 'update', 'partial_update']:
+            return [IsRecruiter()]
+        elif self.action == 'destroy':
+            return [IsAdmin()]
+        return [IsAuthenticated()]
+    
+    def perform_destroy(self, instance):
+        """Soft delete by default."""
+        instance.delete()  # This will soft delete
