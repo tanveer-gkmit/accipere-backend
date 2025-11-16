7ffe1dddfc39bdf60a0bfbad9dbf9914411d0aeb
Tanveer Hussain <tanveer@gkmit.co>
2025-11-16 14:30:41 +0530

    feat(jobs): Add applicants endpoint to retrieve job applications

--- jobs/views.py
+++ jobs/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .serializers import JobSerializer
 from .models import Jobs
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from common.permissions import IsRecruiter, IsAdmin 
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from applications.serializers import ApplicationListSerializer
 
 
 class JobViewset(viewsets.ModelViewSet):
@@ -25,3 +28,11 @@ class JobViewset(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         """Soft delete by default."""
         instance.delete()  # This will soft delete
+    
+    @action(detail=True, methods=['get'], url_path='applicants', permission_classes=[IsRecruiter])
+    def applicants(self, request, pk=None):
+        """Get all applicants for a specific job"""
+        job = self.get_object()
+        applications = job.applications.select_related('job_id', 'current_status').all()
+        serializer = ApplicationListSerializer(applications, many=True)
+        return Response(serializer.data)
